e53a61ea07426aef74b4c455ff4202703a4d4592
Md Arfan Khan <khan188993@gmail.com>
2022-04-05 16:52:15 +0600

    bar chart showing

--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -3,14 +3,15 @@ import { NavLink } from 'react-router-dom'
 
 
 const blog = ({blog}) => {
+    const {img,title,desc,} = blog;
     return (
         <div className="single-product">
-                    <div className="img" style={{backgroundImage:`url(${blog?.img})`}}>
+                    <div className="img" style={{backgroundImage:`url(${img})`}}>
                         <div className="overlay"></div>
                     </div>
                     <div className="content">
-                        <h3>{blog?.title}</h3>
-                        <p>{blog?.desc.length > 30 ? blog?.desc.split(0,30):blog?.desc}</p>
+                        <h3>{title}</h3>
+                        <p>{desc.length > 30 ? desc.split(0,30):desc}</p>
                         {/* eita click krte single route run hobe */}
                         <NavLink to="blogs" className='btn btn-square'>Read More</NavLink>
                     </div>
